54d51cd588070ac5a54ce37d1f4608649cba32be
hounvs <hounvs@gmail.com>
2016-01-25 20:02:12 -0500

    Fix uath path for realsies

--- js/userAuth.js
+++ js/userAuth.js
@@ -2,7 +2,7 @@
     "use strict";
 
     // the main firebase reference
-    var rootRef = new Firebase('https://queueup.firebaseio.com/users/');
+    var rootRef = new Firebase('https://queueup.firebaseio.com/');
 
     // pair our routes to our form elements and controller
     var routeMap = {
